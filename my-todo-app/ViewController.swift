7493b304aeefcade449430953de3fe13903c4fd3
Misaki Shimazaki <msk.toybox@gmail.com>
2022-01-15 12:56:46 +0900

    Initial Commit

---
+++ my-todo-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-todo-app
+//
+//  Created by Misaki Shimazaki on 2022/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ my-todo-appUITests/my_todo_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  my_todo_appUITestsLaunchTests.swift
+//  my-todo-appUITests
+//
+//  Created by Misaki Shimazaki on 2022/01/15.
+//
+
+import XCTest
+
+class my_todo_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
