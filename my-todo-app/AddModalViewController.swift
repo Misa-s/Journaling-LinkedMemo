1798be264438838fc2762ab1f01acd41f361174b
Misaki Shimazaki <msk.toybox@gmail.com>
2022-01-17 09:14:33 +0900

    #2 [WIP] add new TODO on Modal

---
+++ my-todo-app/AddModalViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddModalViewController.swift
+//  my-todo-app
+//
+//  Created by Misaki Shimazaki on 2022/01/15.
+//
+
+import UIKit
+
+protocol PostDelegate: class {
+    func addItem(text: String)
+}
+
+class AddModalViewController: UIViewController {
+    
+    @IBOutlet weak var memo: UITextView!
+    weak var delegate: PostDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        // delegateを使って処理を委任
+        delegate?.addItem(text: memo.text)
+        // モーダルを閉じる
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- my-todo-app/ViewController.swift
+++ my-todo-app/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, PostDelegate {
+    
+    func addItem(text: String) {
+        self.todoList.insert(text, at: 0)
+        self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableView.RowAnimation.right)
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var todoList = [String]()
+    
+    // イニシャライザー的なやつ
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    // UITableViewを継承するとこれ必須の模様, リスト -> cell のアクションなのかな
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        let todoTitle = todoList[indexPath.row]
+        cell.textLabel?.text = todoTitle
+        return cell
+    }
+    
+    @IBAction func addButtonAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "TODO追加", message: "入力して下さい", preferredStyle: UIAlertController.Style.alert)
+        alertController.addTextField(configurationHandler: nil)
+        
+        let okAction = UIAlertAction(title: "追加", style: UIAlertAction.Style.default) { (acrion: UIAlertAction) in
+            // 追加：OKをタップした時の処理
+            if let textField = alertController.textFields?.first {
+                self.todoList.insert(textField.text!, at: 0)
+                self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableView.RowAnimation.right)
+            }
+        }
+        alertController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated:true, completion: nil)
+    }
+    
+    @IBSegueAction func openModalSegueAction(_ coder: NSCoder) -> AddModalViewController? {
+        let modalView = AddModalViewController(coder: coder)
+        modalView?.delegate = self
+        return modalView
+    }
 }
 
