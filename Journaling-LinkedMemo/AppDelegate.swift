f5036253720609420d2ba4d9b3963dfdbc641412
Misa <msk.toybox@gmail.com>
2022-01-30 23:01:18 +0900

    #5 refactor a old app name.

--- Journaling-LinkedMemo/AppDelegate.swift
+++ Journaling-LinkedMemo/AppDelegate.swift
@@ -39,7 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentCloudKitContainer(name: "my_todo_app")
+        let container = NSPersistentCloudKitContainer(name: "Journaling-LinkedMemo")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
